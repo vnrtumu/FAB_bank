e24b979aa9564791060af5f0fa850322e206e4a1
vnrtumu <56113406+vnrtumu@users.noreply.github.com>
2020-01-31 16:08:17 +0530

    init file addition

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -28,6 +28,10 @@ class MerchantController extends Controller
 
     public function store(Request $request)
     {
+        ini_set('max_execution_time', '300');
+        ini_set('memory_limit', '1024M');
+        ini_set('post_max_size', '1024M');
+        ini_set('upload_max_filesize', '1024M');
         $file = $request->file('select_file');
         $merchants = Excel::toArray(new MerchantImport, $file);
         $merchantDetailes  = Excel::toArray(new MerchantDetailImport, $file);
